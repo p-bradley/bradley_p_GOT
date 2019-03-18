5ed1f653780121934be56acbc8a35bc81da87521
p-bradley <p_bradley@fanshaweonline.ca>
2019-03-18 15:20:41 -0400

    Added all videos and created an exit button

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -5,17 +5,31 @@
 	// grab the shields at the bttom of the pge
 	const 	shields 	= document.querySelectorAll('.sigil-container'),
 			lightBox	= document.querySelector('.lightBox'),
-			video = document.querySelector('video');
+			video = document.querySelector('video'),
+			closeLB = document.querySelector('.lightbox-close');
 
 	function showLightbox() {
+
+		let targetHouse = this.className.split(" ")[1];
+
+		let targetSrc = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		video.src = `video/House-${targetSrc}.mp4`;
+
 		lightBox.classList.add('show-lightBox');
+		
+		video.load();
+		video.play();
 	}
 
 	function hideLightbox() {
 		lightBox.classList.remove('show-lightBox');
+		video.currentTime = 0;
+		video.pause();
 	}
 
 	shields.forEach(shield => shield.addEventListener('click', showLightbox));
 
 	video.addEventListener('ended', hideLightbox);
+	closeLB.addEventListener('click', hideLightbox);
 })();
